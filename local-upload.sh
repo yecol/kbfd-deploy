54f4ee7ae8c01f49d1534a111f8fa7cb0dbaa133
Xueli Liu <v1xliu33@haight.inf.ed.ac.uk>
2016-11-10 04:03:18 +0000

    a

--- local-upload.sh
+++ local-upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 file=$1
 des=$2
-#for ((i=1;i<10;i++)); do
-#	scp -r ${file} aws0$i:${des}
-#done
+for ((i=1;i<10;i++)); do
+	scp -r ${file} aws0$i:${des}
+done
 
 for ((i=10;i<=21;i++)); do
 	scp -r ${file} aws$i:${des}
